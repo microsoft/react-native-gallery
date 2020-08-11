6181e2d7e945c3adbe346df0fc78b15ccc8141a5
Keith Melmon <kmelmon@microsoft.com>
2020-08-11 01:01:34 -0700

    hello world

---
+++ windows/rngallery/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::rngallery::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::rngallery::implementation
+
+
