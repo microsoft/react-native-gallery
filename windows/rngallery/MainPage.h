00793f1048f60ba1a2965959f35bebb757375f70
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2020-09-18 13:45:34 -0700

    Update react-native-windows version

---
+++ windows/rngallery/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::rngallery::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::rngallery::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
