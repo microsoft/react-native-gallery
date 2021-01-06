c60a48b9a4f289ec6bad20b5a6c860e3c0bf6de0
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2021-01-06 11:17:08 -0800

    Add Text Page

--- src/components/LinkTile.tsx
+++ src/components/LinkTile.tsx
@@ -3,22 +3,27 @@ import React from 'react';
 import {Hyperlink} from '../components/Hyperlink';
 
 const styles = StyleSheet.create({
-    hyperlinkTile: {
-      marginTop: 30,
-      marginBottom: 10,
-      marginRight: 30,
-    },
-    hyperlinkTileTitle: {
-      marginBottom: 10,
-      fontSize: 20,
-    },
-  });
+  hyperlinkTile: {
+    marginTop: 30,
+    marginBottom: 10,
+    marginRight: 30,
+  },
+  hyperlinkTileTitle: {
+    marginBottom: 10,
+    fontSize: 20,
+  },
+});
 
-  export function LinkTile(props: {title: string; links: [{label: string, url: string}]}) {
-    return (
-        <View style={styles.hyperlinkTile}>
-        <Text style={styles.hyperlinkTileTitle}>{props.title}</Text>
-        {props.links.map(hyp => <Hyperlink label={hyp.label} url={hyp.url}/>)} 
-        </View>
-    );
-  }
+export function LinkTile(props: {
+  title: string;
+  links: {label: string; url: string}[];
+}) {
+  return (
+    <View style={styles.hyperlinkTile}>
+      <Text style={styles.hyperlinkTileTitle}>{props.title}</Text>
+      {props.links.map((hyp) => (
+        <Hyperlink label={hyp.label} url={hyp.url} />
+      ))}
+    </View>
+  );
+}
---
+++ src/examples/TextExamplePage.tsx
@@ -0,0 +1,75 @@
+'use strict';
+import {Text} from 'react-native';
+import React from 'react';
+import {Example} from '../components/Example';
+import {Page} from '../components/Page';
+import {LinkContainer} from '../components/LinkContainer';
+
+export const TextExamplePage: React.FunctionComponent<{}> = () => {
+  const example1jsx = `import {Text} from 'react-native';
+  
+<Text>Here is a line of text.</Text>`;
+  const example2jsx = `import {Text} from 'react-native';
+  
+<Text style={{fontWeight: '600', fontStyle: 'italic'}}>
+  Here is a line of bold and italicized text.
+</Text>`;
+  const example3jsx = `import {Text} from 'react-native';
+  
+<Text style={{color: 'red'}} selectable>
+  Here is a line of bold and italicized text.
+</Text>`;
+  const example4jsx = `import {Text} from 'react-native';
+  
+<Text style={{textAlign: 'center'}} numberOfLines={2}>
+  Here is a sample section of longer text. This sample may take up more
+  that one line if the window width is short enough. Notice that the
+  block of text will never exceed more that two lines. If more text
+  exists beyond two lines it will be truncated and an ellipses will
+  appear.
+</Text>`;
+
+  return (
+    <Page
+      title="Text"
+      description="Text is a component for displaying read-only text in your app.">
+      <Example title="A simple line of Text." code={example1jsx}>
+        <Text>Here is a line of text.</Text>
+      </Example>
+      <Example title="A line of bolded and italicized Text." code={example2jsx}>
+        <Text style={{fontWeight: '600', fontStyle: 'italic'}}>
+          Here is a line of bold and italicized text.
+        </Text>
+      </Example>
+      <Example title="A selectable line of colored Text." code={example3jsx}>
+        <Text style={{color: 'red'}} selectable>
+          Here is a line of bold and italicized text.
+        </Text>
+      </Example>
+      <Example title="A 2-line centered block of Text." code={example4jsx}>
+        <Text style={{textAlign: 'center'}} numberOfLines={2}>
+          Here is a sample section of longer text. This sample may take up more
+          that one line if the window width is short enough. Notice that the
+          block of text will never exceed more that two lines. If more text
+          exists beyond two lines it will be truncated and an ellipses will
+          appear.
+        </Text>
+      </Example>
+      <LinkContainer
+        pageCodeUrl="https://github.com/microsoft/react-native-gallery/blob/main/src/examples/TextExamplePage.tsx"
+        feedbackUrl="https://github.com/microsoft/react-native-gallery/issues/new"
+        documentation={[
+          {
+            label: 'Text',
+            url: 'https://reactnative.dev/docs/text',
+          },
+          {
+            label: 'Text Source Code',
+            url:
+              'https://github.com/microsoft/react-native-windows/blob/master/vnext/Microsoft.ReactNative/Views/TextViewManager.h',
+          },
+        ]}
+      />
+    </Page>
+  );
+};
