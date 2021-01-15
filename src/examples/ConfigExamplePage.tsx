a9621fb92a0ad097e6d9322cc4ecc3af0d18878a
João Matos <joao.matos@janeasystems.com>
2021-01-15 23:15:11 +0000

    Add page for react-native-config component (#25)
    * Add page for react-native-config component
    
    * Address feedback

---
+++ src/examples/ConfigExamplePage.tsx
@@ -0,0 +1,50 @@
+'use strict';
+import {Text} from 'react-native';
+import React from 'react';
+import {Example} from '../components/Example';
+import {Page} from '../components/Page';
+import {LinkContainer} from '../components/LinkContainer';
+import Config from 'react-native-config';
+
+export const ConfigExamplePage: React.FunctionComponent<{}> = () => {
+  const example = `import Config from 'react-native-config';
+
+function Example() {
+  return (
+    <>
+      <Text>Service URL: {Config.SERVICE_URL}</Text>
+      <Text>Service port: {Config.SERVICE_PORT}</Text>
+      <Text>Service API key: {Config.SERVICE_API_KEY}</Text>
+    </>
+  );
+}`;
+
+  const dotenv = `SERVICE_URL=https://example.com
+SERVICE_PORT=8080
+SERVICE_API_KEY=74970b3d-0bfb-4611-ba1d-dff7209ecc39`;
+
+  return (
+    <Page
+      title="Config Variables"
+      description="Shows exposed config variables via the react-native-config module. Keep in mind this module doesn't obfuscate or encrypt secrets for packaging, so do not store sensitive keys in .env.">
+      <Example title="Config Information" code={example}>
+        <Text>Service URL: {Config.SERVICE_URL}</Text>
+        <Text>Service port: {Config.SERVICE_PORT}</Text>
+        <Text>Service API key: {Config.SERVICE_API_KEY}</Text>
+      </Example>
+      <Example title=".env file" code={dotenv}>
+      </Example>
+      <LinkContainer
+        pageCodeUrl="https://github.com/microsoft/react-native-gallery/blob/main/src/examples/ConfigExamplePage.tsx"
+        feedbackUrl="https://github.com/microsoft/react-native-gallery/issues/new"
+        documentation={[
+          {
+            label: 'Config',
+            url:
+              'https://github.com/luggit/react-native-config',
+          },
+        ]}
+      />
+    </Page>
+  );
+};
