6aee245e0d21fecd8dcd54f8283b027e54efe7f5
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2021-02-16 13:13:51 -0800

    Fix Break in Settings Page (#67)
    * Add Webview Page
    
    * Attempt 1 Add OrientationLocker Page
    
    * Revert "Attempt 1 Add OrientationLocker Page"
    
    This reverts commit 4dbf1102ecfba6e5cd790c5be49d702d500eff0d.
    
    * Upgrade WebView Version
    
    * Remove Import Statement
    
    * WebView Redo
    
    * Revert "WebView Redo"
    
    This reverts commit b9b553df1fb476ca5f76bd1b04c437723d786b75.
    
    * Fix CI Loop Issue
    
    * Remove Extra File
    
    * Remove Second Import of WebView Page
    
    * Remove Second Reference to WebView in Solution

--- src/SettingsPage.tsx
+++ src/SettingsPage.tsx
@@ -1,9 +1,8 @@
 'use strict';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import React from 'react';
 import {useGlobal} from 'reactn';
 import {Picker} from '@react-native-picker/picker';
-import {Page} from './components/Page';
 import {Hyperlink} from './components/Hyperlink';
 
 const styles = StyleSheet.create({
@@ -15,6 +14,20 @@ const styles = StyleSheet.create({
   text: {
     paddingTop: 5,
     paddingBottom: 5,
+  },
+  container: {
+    padding: 10,
+    alignSelf: 'stretch',
+    height: '100%',
+  },
+  title: {
+    fontWeight: '200',
+    fontSize: 26,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  scrollView: {
+    paddingRight: 20,
   },
 });
 
@@ -38,70 +51,73 @@ export const SettingsPage: React.FunctionComponent<{}> = () => {
     setTheme(value);
   };
   return (
-    <Page title="Settings">
-      <SettingContainer heading="Theme Mode">
-        <Picker
-          style={{height: 50, width: 200}}
-          onValueChange={PickerValueChanged}
-          selectedValue={theme}>
-          <Picker.Item label="Light" value="light" />
-          <Picker.Item label="Dark" value="dark" />
-          <Picker.Item label="Use System Setting" value="system" />
-        </Picker>
-      </SettingContainer>
-      <SettingContainer heading="About">
-        <Text style={styles.text}>
-          Soure code:{' '}
+    <View style={styles.container}>
+      <Text style={styles.title}>Settings</Text>
+      <ScrollView style={styles.scrollView}>
+        <SettingContainer heading="Theme Mode">
+          <Picker
+            style={{height: 50, width: 200}}
+            onValueChange={PickerValueChanged}
+            selectedValue={theme}>
+            <Picker.Item label="Light" value="light" />
+            <Picker.Item label="Dark" value="dark" />
+            <Picker.Item label="Use System Setting" value="system" />
+          </Picker>
+        </SettingContainer>
+        <SettingContainer heading="About">
+          <Text style={styles.text}>
+            Soure code:{' '}
+            <Hyperlink
+              label="GitHub"
+              url="https://github.com/microsoft/react-native-gallery"
+            />
+          </Text>
+          <Text style={styles.text}>
+            To clone this source repository: git clone
+            https://github.com/microsoft/react-native-gallery
+          </Text>
+          <Text style={styles.text}>Version: 1.0.0.0</Text>
+        </SettingContainer>
+        <SettingContainer heading="Found a bug? Want a new sample?">
+          <Text style={styles.text}>
+            If you have found a bug within the Xaml Controls Gallery, please
+            file an issue on GitHub:
+          </Text>
           <Hyperlink
-            label="GitHub"
-            url="https://github.com/microsoft/react-native-gallery"
+            label="New React Native Gallery Bug Report"
+            url="https://github.com/microsoft/react-native-gallery/issues/new"
           />
-        </Text>
-        <Text style={styles.text}>
-          To clone this source repository: git clone
-          https://github.com/microsoft/react-native-gallery
-        </Text>
-        <Text style={styles.text}>Version: 1.0.0.0</Text>
-      </SettingContainer>
-      <SettingContainer heading="Found a bug? Want a new sample?">
-        <Text style={styles.text}>
-          If you have found a bug within the Xaml Controls Gallery, please file
-          an issue on GitHub:
-        </Text>
-        <Hyperlink
-          label="New React Native Gallery Bug Report"
-          url="https://github.com/microsoft/react-native-gallery/issues/new"
-        />
-        <Text style={styles.text}>
-          If you would like a new sample, request it on Github:
-        </Text>
-        <Hyperlink
-          label="New Sample Request"
-          url="https://github.com/microsoft/react-native-gallery/issues/new"
-        />
-        <Text style={styles.text}>
-          If you have found a bug in your React Native Windows app (not in the
-          React Native Gallery) and need help, file an issue on the React Native
-          Windows GitHub:
-        </Text>
-        <Hyperlink
-          label="New React Native Windows Bug Report"
-          url="https://github.com/microsoft/react-native-windows/issues/new?assignees=&labels=bug&template=bug-report.md&title=Describe+the+problem"
-        />
-      </SettingContainer>
-      <SettingContainer heading="Dependencies and References">
-        <Hyperlink
-          label="React Native for Windows and MacOS Website"
-          url="https://microsoft.github.io/react-native-windows/"
-        />
-        <Hyperlink
-          label="React Native Windows GitHub"
-          url="https://github.com/microsoft/react-native-windows"
-        />
-      </SettingContainer>
-      <SettingContainer heading="Disclaimer">
-        <Text style={styles.text}>INSERT DISCLAIMER INFORMATION</Text>
-      </SettingContainer>
-    </Page>
+          <Text style={styles.text}>
+            If you would like a new sample, request it on Github:
+          </Text>
+          <Hyperlink
+            label="New Sample Request"
+            url="https://github.com/microsoft/react-native-gallery/issues/new"
+          />
+          <Text style={styles.text}>
+            If you have found a bug in your React Native Windows app (not in the
+            React Native Gallery) and need help, file an issue on the React
+            Native Windows GitHub:
+          </Text>
+          <Hyperlink
+            label="New React Native Windows Bug Report"
+            url="https://github.com/microsoft/react-native-windows/issues/new?assignees=&labels=bug&template=bug-report.md&title=Describe+the+problem"
+          />
+        </SettingContainer>
+        <SettingContainer heading="Dependencies and References">
+          <Hyperlink
+            label="React Native for Windows and MacOS Website"
+            url="https://microsoft.github.io/react-native-windows/"
+          />
+          <Hyperlink
+            label="React Native Windows GitHub"
+            url="https://github.com/microsoft/react-native-windows"
+          />
+        </SettingContainer>
+        <SettingContainer heading="Disclaimer">
+          <Text style={styles.text}>INSERT DISCLAIMER INFORMATION</Text>
+        </SettingContainer>
+      </ScrollView>
+    </View>
   );
 };
