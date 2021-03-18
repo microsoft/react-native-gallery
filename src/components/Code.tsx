6298baca4f17ccfb41d4b7c35d7bdeae03d77ee9
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2021-03-18 08:36:54 -0700

    Fix List Warnings for Code Component (#82)
    * Attempt 1 Add OrientationLocker Page
    
    * Revert "Attempt 1 Add OrientationLocker Page"
    
    This reverts commit 4dbf1102ecfba6e5cd790c5be49d702d500eff0d.
    
    * Resolve Same Key Error
    
    * Add Comment for Clarity

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -195,7 +195,19 @@ const lightStyles = StyleSheet.create({
   'hljs-attr': {color: lightColors.text}, // Attributes within JSX Tags
 });
 
-function renderLowLightNode(node: lowlight.AST.Unist.Node) {
+// Text alone does not generate a unique key. Index alone could result in stale UI. Use both to create unique
+// keys that will always ensure up to date UI.
+function createTextKey(text: string, index: number) {
+  return `${index}_${text}`;
+}
+
+// Node alone does not generate a unique key. Index alone could result in stale UI. Use both to create unique
+// keys that will always ensure up to date UI.
+function createElementKey(node: lowlight.AST.Element, index: number) {
+  return `${index}_${node}`;
+}
+
+function renderLowLightNode(node: lowlight.AST.Unist.Node, index: number) {
   //@ts-ignore
   const [theme, setTheme] = useGlobal('theme');
   const themeStyles =
@@ -208,7 +220,7 @@ function renderLowLightNode(node: lowlight.AST.Unist.Node) {
       : 0;
   if (node.type === 'text') {
     let text = (node as lowlight.AST.Text).value;
-    return <Text key={text}>{text}</Text>;
+    return <Text key={createTextKey(text, index)} >{text}</Text>;
   } else if (node.type === 'element') {
     const elementNode = node as lowlight.AST.Element;
     if (themeStyles === 0) {
@@ -230,7 +242,7 @@ function renderLowLightNode(node: lowlight.AST.Unist.Node) {
           ]
         : {};
       return (
-        <Text key={elementNode.tagName} style={style}>
+        <Text key={createElementKey(elementNode, index)} style={style}>
           {elementNode.children.map(renderLowLightNode)}
         </Text>
       );
@@ -251,7 +263,7 @@ function renderLowLightNode(node: lowlight.AST.Unist.Node) {
           ]
         : {};
       return (
-        <Text key={elementNode.tagName} style={style}>
+        <Text key={createElementKey(elementNode, index)} style={style}>
           {elementNode.children.map(renderLowLightNode)}
         </Text>
       );
@@ -263,7 +275,7 @@ function renderLowLightNode(node: lowlight.AST.Unist.Node) {
 
 function renderLowLightTree(tree: lowlight.HastNode[]) {
   if (tree.length === 1) {
-    return renderLowLightNode(tree[0]);
+    return renderLowLightNode(tree[0], 0);
   } else {
     return <>{tree.map(renderLowLightNode)}</>;
   }
