5a6aeb1026a70cce8f92f3963089a359e5960151
João Matos <joao@tritao.eu>
2021-03-29 19:48:35 +0100

    Add example for react-native-gesture-handler component (#87)

--- windows/rngallery/AutolinkedNativeModules.g.cpp
+++ windows/rngallery/AutolinkedNativeModules.g.cpp
@@ -15,6 +15,9 @@
 // Includes from react-native-device-info
 #include <winrt/RNDeviceInfoCPP.h>
 
+// Includes from react-native-gesture-handler
+#include <winrt/RNGestureHandler.h>
+
 // Includes from react-native-permissions
 #include <winrt/RNPermissions.h>
 
@@ -37,6 +40,8 @@ void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collecti
     packageProviders.Append(winrt::RNCConfig::ReactPackageProvider());
     // IReactPackageProviders from react-native-device-info
     packageProviders.Append(winrt::RNDeviceInfoCPP::ReactPackageProvider());
+    // IReactPackageProviders from react-native-gesture-handler
+    packageProviders.Append(winrt::RNGestureHandler::ReactPackageProvider());
     // IReactPackageProviders from react-native-permissions
     packageProviders.Append(winrt::RNPermissions::ReactPackageProvider());
     // IReactPackageProviders from react-native-print
