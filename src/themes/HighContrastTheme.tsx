b39131e3011598492455bf6f22c5e18cbbb52bf7
Agne Lukoseviciute <agnel@microsoft.com>
2022-08-15 10:37:58 -0700

    updated theme file

--- src/themes/HighContrastTheme.tsx
+++ src/themes/HighContrastTheme.tsx
@@ -4,11 +4,11 @@ import {AppTheme} from 'react-native-windows';
 const HighContrastTheme: Theme = {
   dark: false,
   colors: {
-    primary: AppTheme.currentHighContrastColors.ButtonFaceColor,
+    primary: AppTheme.currentHighContrastColors.HighlightColor,
     background: AppTheme.currentHighContrastColors.WindowColor,
     card: AppTheme.currentHighContrastColors.WindowColor,
     text: AppTheme.currentHighContrastColors.WindowTextColor,
-    border: AppTheme.currentHighContrastColors.HighlightColor,
+    border: AppTheme.currentHighContrastColors.ButtonTextColor,
     notification: AppTheme.currentHighContrastColors.HotlightColor,
   },
 };
