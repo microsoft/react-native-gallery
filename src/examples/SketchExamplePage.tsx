005f85d6b79fbd8d9e78599514d09fc19d7ed707
Yajur Grover <yajurgrover@microsoft.com>
2023-04-12 11:17:35 -0700

    linting fix pt 2

--- src/examples/SketchExamplePage.tsx
+++ src/examples/SketchExamplePage.tsx
@@ -27,7 +27,9 @@ export const SketchExamplePage: React.FunctionComponent<{}> = () => {
   </View>
 </View>`;
 
-  const sketchRef: React.RefObject<SketchCanvas> = React.createRef<SketchCanvas>();
+  const sketchRef: React.RefObject<SketchCanvas> = React.createRef<
+    SketchCanvas
+  >();
 
   return (
     <Page
