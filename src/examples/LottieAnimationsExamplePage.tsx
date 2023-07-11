2d970aeb8c86eaa9e78609b192e2bf8cbc6e709e
Victor Guilherme <92493696+Victor0814gui@users.noreply.github.com>
2023-07-11 04:45:13 -0300

    Update LottieAnimationsExamplePage.tsx

--- src/examples/LottieAnimationsExamplePage.tsx
+++ src/examples/LottieAnimationsExamplePage.tsx
@@ -1,9 +1,9 @@
 'use strict';
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { Button, Platform, View } from 'react-native';
-import { Example } from '../components/Example';
-import { Page } from '../components/Page';
-import LottieView, { AnimatedLottieViewProps } from 'lottie-react-native';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
+import {Button, Platform, View} from 'react-native';
+import {Example} from '../components/Example';
+import {Page} from '../components/Page';
+import LottieView, {AnimatedLottieViewProps} from 'lottie-react-native';
 
 export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
   const [loop, setLoop] = useState(true);
@@ -36,19 +36,19 @@ export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
 
   const onPlay = useCallback(() => {
     anim.current?.play();
-  }, [])
+  }, []);
 
   const onResume = useCallback(() => {
     anim.current?.resume()
-  }, [])
+  }, []);
 
   const onPause = useCallback(() => {
     anim.current?.pause();
-  }, [])
+  }, []);
 
   const onReset = useCallback(() => {
     anim.current?.reset();
-  }, [])
+  }, []);
 
 
   const handlerChangeLoop = () => {
@@ -56,7 +56,7 @@ export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
       anim.current?.play();
     }
     setLoop(!loop)
-  }
+  };
 
   useEffect(() => {
     anim.current?.play();
@@ -64,47 +64,47 @@ export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
 
   return (
     <Page
-      title='Lottie animations'
-      description='Lottie é um ecossistema de bibliotecas para analisar animações do Adobe After Effects exportadas como JSON com bodymovin e renderizá-las nativamente!.'
-      componentType='Community'
-      pageCodeUrl='https://github.com/microsoft/react-native-gallery/blob/main/src/examples/WindowsHelloExamplePage.tsx'
+      title="Lottie animations"
+      description="Lottie é um ecossistema de bibliotecas para analisar animações do Adobe After Effects exportadas como JSON com bodymovin e renderizá-las nativamente!."
+      componentType="Community"
+      pageCodeUrl="https://github.com/microsoft/react-native-gallery/blob/main/src/examples/WindowsHelloExamplePage.tsx"
       documentation={[
         {
           label: 'Lottie React Native',
           url: 'https://github.com/lottie-react-native/lottie-react-native',
         },
       ]}>
-      <Example title='A simple Lottie animation.' code={example1jsx}>
+      <Example title="A simple Lottie animation." code={example1jsx}>
         <LottieView
           autoPlay
           resizeMode={'cover'}
           source={'LottieLogo'}
           style={{
             height: 120,
-            width: 120
+            width: 120,
           }}
         />
       </Example>
       <Example
-        title='A colored and rounded View with specified height and width.'
+        title="A colored and rounded View with specified height and width."
         code={example2jsx}>
         <LottieView
           ref={anim}
-          resizeMode='contain'
+          resizeMode="contain"
           source={
             Platform.select({
               windows: 'Loading Atom Colored',
-              default: require('../assets/animations/Loading-Atom-Colored.json')
+              default: require('../assets/animations/Loading-Atom-Colored.json'),
             })
           }
           autoPlay={false}
           loop={loop}
           style={{
             height: 120,
-            width: 120
+            width: 120,
           }}
         />
-        <View style={{ flexDirection: 'row', gap: 12 }}>
+        <View style={{flexDirection: 'row', gap: 12}}>
           <Button onPress={onPlay} title={'play'} />
           <Button onPress={onPause} title={'pause'} />
           <Button onPress={onResume} title={'resume'} />
