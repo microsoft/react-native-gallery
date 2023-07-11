51d7e6c226b8ff7d49c737117b5f7f5b4391d05d
Victor Guilherme <92493696+Victor0814gui@users.noreply.github.com>
2023-07-10 22:47:37 -0300

    Update LottieAnimationsExamplePage.tsx

--- src/examples/LottieAnimationsExamplePage.tsx
+++ src/examples/LottieAnimationsExamplePage.tsx
@@ -1,18 +1,18 @@
-'use strict';
-import React, {useState,useEffect,useRef,useCallback} from 'react';
-import {Button,Platform,View} from 'react-native';
-import {Example} from '../components/Example';
-import {Page} from '../components/Page';
-import LottieView, {AnimatedLottieViewProps} from 'lottie-react-native';
+"use strict";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { Button, Platform, View } from "react-native";
+import { Example } from "../components/Example";
+import { Page } from "../components/Page";
+import LottieView, { AnimatedLottieViewProps } from "lottie-react-native";
 
 export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
   const [loop, setLoop] = useState(true);
-  useState<AnimatedLottieViewProps['renderMode']>('SOFTWARE');
+  useState<AnimatedLottieViewProps["renderMode"]>("SOFTWARE");
   const anim = useRef<LottieView>(null);
 
   const example1jsx = `
     <LottieView 
-      source={'MyAnimation'}
+      source={"MyAnimation"}
       autoPlay
       style={{height: 120, width: 120}}
     >
@@ -20,11 +20,11 @@ export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
   const example2jsx = `<View
   <LottieView
     ref={anim}
-    resizeMode='contain' 
+    resizeMode="contain" 
     source={
       Platform.select({
-        windows:'Loading Atom Colored',
-        default: require('../assets/animations/Loading-Atom-Colored.json')
+        windows:"Loading Atom Colored",
+        default: require("../assets/animations/Loading-Atom-Colored.json")
       })
     } 
     autoPlay={false}
@@ -37,23 +37,23 @@ export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
 
   const onPlay = useCallback(() => {
     anim.current?.play();
-  },[])
+  }, [])
 
   const onResume = useCallback(() => {
     anim.current?.resume()
-  },[])
+  }, [])
 
   const onPause = useCallback(() => {
     anim.current?.pause();
-  },[])
+  }, [])
 
   const onReset = useCallback(() => {
     anim.current?.reset();
-  },[])
+  }, [])
 
 
   const handlerChangeLoop = () => {
-    if(!loop){
+    if (!loop) {
       anim.current?.play();
     }
     setLoop(!loop)
@@ -64,40 +64,40 @@ export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
   }, []);
 
   return (
-    <Page    
-      title='Lottie animations'
-      description='Lottie é um ecossistema de bibliotecas para analisar animações do Adobe After Effects exportadas como JSON com bodymovin e renderizá-las nativamente!.'
-      componentType='Community'
-      pageCodeUrl='https://github.com/microsoft/react-native-gallery/blob/main/src/examples/WindowsHelloExamplePage.tsx'
+    <Page
+      title="Lottie animations"
+      description="Lottie é um ecossistema de bibliotecas para analisar animações do Adobe After Effects exportadas como JSON com bodymovin e renderizá-las nativamente!."
+      componentType="Community"
+      pageCodeUrl="https://github.com/microsoft/react-native-gallery/blob/main/src/examples/WindowsHelloExamplePage.tsx"
       documentation={[
         {
-          label: 'Lottie React Native',
-          url: 'https://github.com/lottie-react-native/lottie-react-native',
+          label: "Lottie React Native",
+          url: "https://github.com/lottie-react-native/lottie-react-native",
         },
       ]}>
-      <Example title='A simple Lottie animation.' code={example1jsx}>
+      <Example title="A simple Lottie animation." code={example1jsx}>
         <LottieView
           autoPlay
-          resizeMode={'cover'}
-          source={'LottieLogo'} 
+          resizeMode={"cover"}
+          source={"LottieLogo"}
           style={{
             height: 120,
             width: 120
           }}
         />
       </Example>
-      <Example      
-        title='A colored and rounded View with specified height and width.'
+      <Example
+        title="A colored and rounded View with specified height and width."
         code={example2jsx}>
         <LottieView
           ref={anim}
-          resizeMode='contain' 
+          resizeMode="contain"
           source={
             Platform.select({
-              windows: 'Loading Atom Colored',
-              default: require('../assets/animations/Loading-Atom-Colored.json')
+              windows: "Loading Atom Colored",
+              default: require("../assets/animations/Loading-Atom-Colored.json")
             })
-          } 
+          }
           autoPlay={false}
           loop={loop}
           style={{
@@ -105,15 +105,14 @@ export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
             width: 120
           }}
         />
-       <View style={{flexDirection:'row',gap: 12}}>
-          <Button onPress={onPlay} title={'play'}/>
-          <Button onPress={onPause} title={'pause'}/>
-          <Button onPress={onResume} title={'resume'}/>
-          <Button onPress={onReset} title='reset' />
-          <Button onPress={handlerChangeLoop} title={loop ? 'disable loop' : 'ative resume'}/>
+        <View style={{ flexDirection: "row", gap: 12, }}>
+          <Button onPress={onPlay} title={"play"} />
+          <Button onPress={onPause} title={"pause"} />
+          <Button onPress={onResume} title={"resume"} />
+          <Button onPress={onReset} title="reset" />
+          <Button onPress={handlerChangeLoop} title={loop ? "disable loop" : "ative resume"} />
         </View>
       </Example>
     </Page>
   );
 };
- 
