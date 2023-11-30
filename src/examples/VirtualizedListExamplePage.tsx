5254d3ab4e968498a487107d36a76d2cabcea228
Yajur Grover <yajurgrover24@gmail.com>
2023-11-29 16:58:34 -0800

    added a11yLabel

--- src/examples/VirtualizedListExamplePage.tsx
+++ src/examples/VirtualizedListExamplePage.tsx
@@ -371,6 +371,7 @@ export const VirtualizedListExamplePage: React.FunctionComponent<{}> = () => {
           <View style={styles.selectionContainer}>
             <Text>Selection Support</Text>
             <Picker
+              accessibilityLabel='Selection Support'
               style={{height: 50, width: 200, marginTop: 10}}
               itemStyle={{color: colors.text}}
               selectedValue={selectedSupport}
