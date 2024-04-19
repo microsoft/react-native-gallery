f33ab1ea284987edd3f66aa99391a1e991ec40c5
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2024-04-19 11:28:59 -0700

    Add "copy to clipboard" button to code blocks Also update visual style to closer match WinUI3 gallery

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Code} from './Code';
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, PlatformColor, Text, View} from 'react-native';
 import {useTheme} from '@react-navigation/native';
+import Clipboard from '@react-native-clipboard/clipboard';
 
 const createStyles = (colors: any) =>
   StyleSheet.create({
@@ -12,19 +13,77 @@ const createStyles = (colors: any) =>
       color: colors.text,
     },
     box: {
-      borderRadius: 2,
+      borderRadius: 8,
       borderWidth: 1,
       borderColor: colors.border,
     },
     exampleContainer: {
       padding: 15,
+      backgroundColor: PlatformColor('SolidBackgroundFillColorSecondaryBrush'),
     },
     codeContainer: {
       borderWidth: 0,
       borderTopWidth: 1,
       borderColor: colors.border,
+      padding: 12,
+      backgroundColor: PlatformColor('CardBackgroundFillColorDefaultBrush'),
+    },
+    copyButton: {
+      borderRadius: 8,
+      borderWidth: 1,
+      borderColor: colors.border,
+      padding: 6,
+    },
+    copyButtonText: {
+      fontFamily: 'Segoe MDL2 Assets',
+      fontSize: 16,
+      color: PlatformColor('TextControlForeground'),
     },
   });
+
+const createButtonStyles = (isHovered: boolean, isPressing: boolean) =>
+  StyleSheet.create({
+    background: {
+      backgroundColor: isPressing
+        ? PlatformColor('ControlFillColorTertiaryBrush')
+        : isHovered
+        ? PlatformColor('ControlFillColorSecondaryBrush')
+        : PlatformColor('ControlFillColorDefaultBrush'),
+      borderRadius: 8,
+      borderWidth: 1,
+      borderColor: PlatformColor('ControlStrokeColorDefaultBrush'),
+      padding: 6,
+    },
+    text: {
+      fontFamily: 'Segoe MDL2 Assets',
+      fontSize: 16,
+      color: PlatformColor('TextControlForeground'),
+    },
+  });
+
+type CopyToClipboardButtonProps = {
+  content: string;
+};
+const CopyToClipboardButton = ({content}: CopyToClipboardButtonProps) => {
+  const [isHovered, setIsHovered] = React.useState(false);
+  const [isPressing, setIsPressing] = React.useState(false);
+  const styles = createButtonStyles(isHovered, isPressing);
+
+  const copyIcon = '\uE8C8';
+
+  return (
+    <Pressable
+      tooltip={'Copy to clipboard'}
+      style={styles.background}
+      onPress={() => Clipboard.setString(content)}
+      onPressIn={() => setIsPressing(true)}
+      onPressOut={() => setIsPressing(false)}
+      onHoverIn={() => setIsHovered(true)}
+      onHoverOut={() => setIsHovered(false)}>
+      <Text style={styles.text}>{copyIcon}</Text>
+    </Pressable>
+  );
+};
 
 export function Example(props: {
   title: string;
@@ -43,6 +102,9 @@ export function Example(props: {
           <View style={styles.exampleContainer}>{props.children}</View>
           <View style={styles.codeContainer}>
             <Code>{props.code}</Code>
+            <View style={{position: 'absolute', right: 12, top: 12}}>
+              <CopyToClipboardButton content={props.code} />
+            </View>
           </View>
         </View>
       ) : (
