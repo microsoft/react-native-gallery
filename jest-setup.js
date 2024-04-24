52b5a3087300226eb8cab8a0f083a60168472aaf
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2024-04-24 14:53:54 -0700

    Fix linting

--- jest-setup.js
+++ jest-setup.js
@@ -63,36 +63,37 @@ jest.mock('@react-navigation/native', () => {
 // For NetworkExamplePage.tsx
 global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () => Promise.resolve({
-      "title": "The Basics - Networking",
-      "description": "Your app fetched this from a remote endpoint!",
-      "movies": [
+    json: () =>
+      Promise.resolve({
+        title: 'The Basics - Networking',
+        description: 'Your app fetched this from a remote endpoint!',
+        movies: [
           {
-              "id": "1",
-              "title": "Star Wars",
-              "releaseYear": "1977"
+            id: '1',
+            title: 'Star Wars',
+            releaseYear: '1977',
           },
           {
-              "id": "2",
-              "title": "Back to the Future",
-              "releaseYear": "1985"
+            id: '2',
+            title: 'Back to the Future',
+            releaseYear: '1985',
           },
           {
-              "id": "3",
-              "title": "The Matrix",
-              "releaseYear": "1999"
+            id: '3',
+            title: 'The Matrix',
+            releaseYear: '1999',
           },
           {
-              "id": "4",
-              "title": "Inception",
-              "releaseYear": "2010"
+            id: '4',
+            title: 'Inception',
+            releaseYear: '2010',
           },
           {
-              "id": "5",
-              "title": "Interstellar",
-              "releaseYear": "2014"
-          }
-      ]
+            id: '5',
+            title: 'Interstellar',
+            releaseYear: '2014',
+          },
+        ],
+      }),
   }),
-  })
 );
