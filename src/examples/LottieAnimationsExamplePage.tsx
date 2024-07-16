1d58109edae05281d226da42ebeea1c027e015bc
Yajur Grover <yajurgrover24@gmail.com>
2024-07-16 12:00:15 -0700

    fix lottie a11yLabel issues

--- src/examples/LottieAnimationsExamplePage.tsx
+++ src/examples/LottieAnimationsExamplePage.tsx
@@ -100,13 +100,22 @@ export const LottieAnimationsExamplePage: React.FunctionComponent<{}> = () => {
           }}
         />
         <View style={{flexDirection: 'row', gap: 12}}>
-          <Button onPress={onPlay} title={'play'} />
-          <Button onPress={onPause} title={'pause'} />
-          <Button onPress={onResume} title={'resume'} />
-          <Button onPress={onReset} title="reset" />
+          <Button onPress={onPlay} title={'play'} accessibilityLabel="Play" />
+          <Button
+            onPress={onPause}
+            title={'pause'}
+            accessibilityLabel="Pause"
+          />
+          <Button
+            onPress={onResume}
+            title={'resume'}
+            accessibilityLabel="Resume"
+          />
+          <Button onPress={onReset} title="reset" accessibilityLabel="Reset" />
           <Button
             onPress={handlerChangeLoop}
             title={loop ? 'disable loop' : 'ative resume'}
+            accessibilityLabel="Disable Loop"
           />
         </View>
       </Example>
