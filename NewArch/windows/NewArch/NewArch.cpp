d7902ad1d36090085f7b0d9ece473508a840a032
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-06-06 15:28:06 +0530

    Fix Title Bar Bug in New Arch Gallery App

--- NewArch/windows/NewArch/NewArch.cpp
+++ NewArch/windows/NewArch/NewArch.cpp
@@ -70,7 +70,7 @@ _Use_decl_annotations_ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, PSTR 
 
   // Get the AppWindow so we can configure its initial title and size
   auto appWindow{reactNativeWin32App.AppWindow()};
-  appWindow.Title(L"NewArch");
+  appWindow.Title(L"React Native Gallery - Experimental");
   appWindow.Resize({1000, 1000});
 
   // Get the ReactViewOptions so we can set the initial RN component to load
