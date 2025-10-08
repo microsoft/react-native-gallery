dbb5e94264a16c91c460dd4a8b65f498823ac8b5
Protik Biswas <protikbiswas100@microsoft.com>
2025-10-08 13:44:41 +0530

    removing accessibilityInfo

--- NewArch/src/components/AccessibilityNavigationHelper.tsx
+++ NewArch/src/components/AccessibilityNavigationHelper.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, AccessibilityInfo } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 interface AccessibilitySkipLinkProps {
   label: string;
