ceb7ab9ba5a423177d313ec571d6a3369230c1b9
Protik Biswas <219775028+protikbiswas100@users.noreply.github.com>
2025-10-15 13:18:32 +0530

    Landmarks are not defined in the React Native Gallery Preview app (#721)
    * removing unnecessray statement by narrator on clicking hamburger button
    
    * adding landmark for narrator
    
    * adding lanmark pane
    
    * removing unnecessary files
    
    * Revert windows build artifacts and package version changes
    
    - Revert NewArch/windows/NewArch.Package/packages.lock.json to previous version
    - Remove msbuild error and warning files that were generated during builds
    - Clean up windows folder from unnecessary build artifacts
    
    * removing additional lines
    
    * removing extra
    
    * removing spaces
    
    * fixing lint error
    
    * removing extra
    
    * removing accessibilityInfo
    
    * adding jest fix
    
    * adding checks
    
    * Revert "adding checks"
    
    This reverts commit 9bfb6711a4df014722dbbfc0a66d945ebf6f4556.
    
    * Fix Jest configuration to make tests pass
    
    - Add transformIgnorePatterns for react-native modules
    - Add setupFilesAfterEnv to load mocks
    - Add moduleNameMapper for react-native-windows
    - Fixes clipboard and other module mocking issues
    
    * fixing lint issue
    
    ---------
    
    Co-authored-by: Protik Biswas <protikbiswas100@microsoft.com>

--- NewArch/jest.config.js
+++ NewArch/jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-windows)/)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  moduleNameMapper: {
+    '^react-native-windows$': '<rootDir>/__mocks__/react-native-windows.js',
+  },
 };
