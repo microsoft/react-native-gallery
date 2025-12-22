0d9da821ef82ebdbeee59bba5b4d107fcc8fd2d3
vineethkuttan <66076509+vineethkuttan@users.noreply.github.com>
2025-12-22 12:22:56 +0530

    Upgrade Paper Gallery to 0.81

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,6 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 const fs = require('fs');
 const path = require('path');
-
 const rnwPath = fs.realpathSync(
   path.resolve(require.resolve('react-native-windows/package.json'), '..'),
 );
